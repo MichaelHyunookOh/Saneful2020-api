305baf5112c51ed551cac0736275bce7fd9ef4f5
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-09-09 15:02:27 -0700

    post /save game endpoint

--- src/saved-games/saved-game-router.js
+++ src/saved-games/saved-game-router.js
@@ -4,13 +4,54 @@ const SavedGameService = require("./saved-game-services");
 const { requireAuth } = require("../auth/jwt-auth");
 
 const SavedGameRouter = express.Router();
-const jsonBodyParser = express.json();
+const jsonParser = express.json();
 
 SavedGameRouter
-.route("/").get(requireAuth, (req, res, next) => {
+  .route("/")
+  .get(requireAuth, (req, res, next) => {
     SavedGameService.getSavedGame(req.app.get("db"), req.user.user_id)
       .then((savedGame) => {
-        res.json(savedGame.map(game => InventoryService.serializeInventory(game)));
+        res.json(savedGame.map(game => SavedGameService.serializeSavedGame(game)));
+      })
+      .catch(next);
+  })
+  .post(requireAuth, jsonParser, (req, res, next) => {
+    const {
+      current_x_coord,
+      current_y_coord,
+      money_counter,
+      health_points,
+      sanity_points,
+      energy_points,
+      elapsed_time,
+    } = req.body;
+    const newSave = {
+        current_x_coord,
+        current_y_coord,
+        money_counter,
+        health_points,
+        sanity_points,
+        energy_points,
+        elapsed_time,
+    };
+
+    for (const [key, value] of Object.entries(newSave)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+      }
+    }
+    newSave.user_id = req.user.user_id;
+    console.log(newSave)
+
+    SavedGameService.insertSave(req.app.get('db'), newSave)
+      .then((save) => {
+          console.log(save)
+        res
+          .status(201)
+          .location(`/api/save/${save.id}`)
+          .json(SavedGameService.serializeSavedGame(save));
       })
       .catch(next);
   });
--- src/saved-games/saved-game-services.js
+++ src/saved-games/saved-game-services.js
@@ -1,6 +1,34 @@
 const xss = require("xss");
 
 const SavedGameService = {
+    getAllSavedGames(db) {
+        return db
+        .from('saneful_saved_game AS sav')
+        .select(
+          'sav.saved_game_id',
+          'sav.current_x_coord',
+          'sav.current_y_coord',
+          'sav.money_counter',
+          'sav.health_points',
+          'sav.sanity_points',
+          'sav.energy_points',
+          'sav.elapsed_time',
+          
+          db.raw(
+            `json_strip_nulls(
+              json_build_object(
+                'user_id', usr.user_id,
+                'user_name', usr.user_name,
+                'user_email', usr.user_email,
+                'date_created', usr.date_created
+              )
+            ) AS "user"`
+          )
+        )
+        .leftJoin('saneful_user AS usr', 'sav.user_id', 'usr.user_id')
+        .groupBy('sav.saved_game_id', 'usr.user_id');
+        },
+    
     getSavedGame(db, id) {
     return db
     .from('saneful_saved_game AS sav')
@@ -10,9 +38,9 @@ const SavedGameService = {
       'sav.current_y_coord',
       'sav.money_counter',
       'sav.health_points',
-      'sav_sanity_points',
-      'sav_energy_points',
-      'sav_elapsed_time',
+      'sav.sanity_points',
+      'sav.energy_points',
+      'sav.elapsed_time',
       
       db.raw(
         `json_strip_nulls(
@@ -27,7 +55,7 @@ const SavedGameService = {
     )
     .leftJoin('saneful_user AS usr', 'sav.user_id', 'usr.user_id')
     .where('usr.user_id', id)
-    .groupBy('inv.inventory_id', 'usr.user_id');
+    .groupBy('sav.saved_game_id', 'usr.user_id');
     },
 
     serializeSavedGame(save) {
@@ -41,13 +69,28 @@ const SavedGameService = {
           sanity_points: save.sanity_points,
           energy_points: save.energy_points,
           elapsed_time: save.elapsed_time,
-          user: {
-            user_id: user.user_id,
-            user_name: user.user_name,
-            user_email: user.user_email,
-            date_created: user.date_created
-          },
+        //   user: {
+        //     user_id: user.user_id,
+        //     user_name: user.user_name,
+        //     user_email: user.user_email,
+        //     date_created: user.date_created
+        //   },
         };
+      },
+
+    getById(db, id) {
+        return SavedGameService.getAllSavedGames(db)
+          .where('sav.saved_game_id', id)
+          .first();
+      },
+
+    insertSave(db, newSave) {
+        return db
+          .insert(newSave)
+          .into('saneful_saved_game')
+          .returning('*')
+          .then(([row]) => row)
+        //   .then((row) => SavedGameService.getById(db, row.id));
       },
 };
 
