215b39ebd72e23030999be9944a082e780a6987d
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-09-21 12:09:25 -0700

    auth

--- test/auth.test.js
+++ test/auth.test.js
@@ -5,7 +5,7 @@ const helpers = require('./test-helpers');
 const supertest = require('supertest');
 const jwt = require('jsonwebtoken');
 
-describe('Auth Endpoints', () => {
+describe.only('Auth Endpoints', () => {
   let db;
   
   const { testUsers } = helpers.makeSavesFixtures();
@@ -14,18 +14,22 @@ describe('Auth Endpoints', () => {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     })
     app.set('db',db);
   });
 
   after('disconnect from db', () => db.destroy());
 
-  before('cleanup', () => helpers.cleanTables(db));
+  before('clean the table', () =>
+    db.raw('TRUNCATE saneful_saved_game, saneful_user RESTART IDENTITY CASCADE')
+  );
 
-  afterEach('cleanup', () => helpers.cleanTables(db));
+  afterEach('clean the table', () =>
+    db.raw('TRUNCATE saneful_saved_game, saneful_user RESTART IDENTITY CASCADE')
+  );
 
-  describe('POST /api/auth/login', () => {
+  describe('POST /api/auth', () => {
     beforeEach('insert users', () => {
       helpers.seedUsers(
         db,
@@ -33,60 +37,61 @@ describe('Auth Endpoints', () => {
       )
     })
 
-    const requiredFields = ['user_name', 'password'];
+    const requiredFields = ['user_email', 'user_password'];
 
     requiredFields.forEach(field => {
       const loginAttemptBody = {
-        user_name: testUser.user_name,
-        password: testUser.password
+        user_email: testUser.user_email,
+        user_password: testUser.user_password
       }
 
       it(`responds with 400 required error when '${field}' is missing`, () => {
         delete loginAttemptBody[field];
 
         return supertest(app)
-          .post('/api/auth/login')
+          .post('/api/auth')
           .send(loginAttemptBody)
-          .expect(400, {
-            error: `Missing '${field}' in request body`
-          })
+          .expect(400)
       })
     })
 
-    it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
-      const userInvalidUser = { user_name: 'user-not', password: 'existy' }
+    it(`responds 400 'invalid email or password' when bad user_name`, () => {
+      const userInvalidUser = { user_email: 'user-not', user_password: 'existy' }
       return supertest(app)
-        .post('/api/auth/login')
+        .post('/api/auth')
         .send(userInvalidUser)
-        .expect(400, { error: `Incorrect user_name or password` })
+        .expect(400, { error: `Incorrect email or password` })
     })
 
-    it(`responds 400 'invalid user_name or password' when bad password`, () => {
-      const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
+    it(`responds 400 'invalid email or password' when bad password`, () => {
+      const userInvalidPass = { user_email: testUser.user_email, user_password: 'incorrect' }
       return supertest(app)
-        .post('/api/auth/login')
+        .post('/api/auth')
         .send(userInvalidPass)
-        .expect(400, { error: `Incorrect user_name or password` })
+        .expect(400, { error: `Incorrect email or password` })
     })
 
     it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
       const userValidCreds = {
-        user_name: testUser.user_name,
-        password: testUser.password,
+        user_email: testUser.user_email,
+        user_password: testUser.user_password,
       }
       const expectedToken = jwt.sign(
-        { user_id: testUser.id },
+        { user_id: testUser.user_id, user_name: testUser.user_name },
         process.env.JWT_SECRET,
         {
-          subject: testUser.user_name,
+          subject: testUser.user_email,
           algorithm: 'HS256',
         }
       )
+      console.log(expectedToken)
       return supertest(app)
-        .post('/api/auth/login')
+        .post('/api/auth')
         .send(userValidCreds)
         .expect(200, {
           authToken: expectedToken,
+          user_id: testUser.user_id,
+          user_name: testUser.user_name
         })
     })
   })
