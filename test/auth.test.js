2aeced1d2df732bba95475f76a4c6c641412aaa1
nickthorpe71 <nickthorpe71@gmail.com>
2020-09-22 08:50:48 -0500

    accepting merge

--- test/auth.test.js
+++ test/auth.test.js
@@ -58,42 +58,26 @@ describe.only('Auth Endpoints', () => {
           .expect(400);
       });
     });
-<<<<<<< HEAD
-
-    it(`responds 400 'invalid email or password' when bad user_name`, () => {
-      const userInvalidUser = { user_email: 'user-not', user_password: 'existy' };
-=======
     it(`responds 400 'invalid email or password' when bad user_name`, () => {
       const userInvalidUser = {
         user_email: 'user-not',
         user_password: 'existy'
       };
->>>>>>> f07ef372bb475f34a776d8c10447cb8d56388a7e
       return supertest(app)
         .post('/api/auth')
         .send(userInvalidUser)
         .expect(400, { error: `Incorrect email or password` });
     });
-<<<<<<< HEAD
-
-    it(`responds 400 'invalid email or password' when bad password`, () => {
-      const userInvalidPass = { user_email: testUser.user_email, user_password: 'incorrect' };
-=======
     it(`responds 400 'invalid email or password' when bad password`, () => {
       const userInvalidPass = {
         user_email: testUser.user_email,
         user_password: 'incorrect'
       };
->>>>>>> f07ef372bb475f34a776d8c10447cb8d56388a7e
       return supertest(app)
         .post('/api/auth')
         .send(userInvalidPass)
         .expect(400, { error: `Incorrect email or password` });
     });
-<<<<<<< HEAD
-
-=======
->>>>>>> f07ef372bb475f34a776d8c10447cb8d56388a7e
     it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
       const userValidCreds = {
         user_email: testUser.user_email,
@@ -107,24 +91,6 @@ describe.only('Auth Endpoints', () => {
           algorithm: 'HS256'
         }
       );
-<<<<<<< HEAD
-      console.log('----------', expectedToken);
-      console.log(userValidCreds);
-      return supertest(app)
-        .post('/api/auth')
-        .set('Content-Type', 'application/json')
-        .send(userValidCreds)
-        .expect(200)
-        .then(res => {
-          expect(res.body.authToken).to.equal(expectedToken);
-          // expect(res.body.user.user_id).to.equal(user.user_id);
-          // expect(res.body.user.user_email).to.equal(user.user_email);
-          expect(res.body.user.user_password).to.be.undefined;
-        });
-    });
-  });
-});
-=======
       const expectedId = testUser.user_id;
       const expectedUser = testUser.user_name;
       return supertest(app).post('/api/auth').send(userValidCreds).expect(200, {
@@ -135,4 +101,3 @@ describe.only('Auth Endpoints', () => {
     });
   });
 });
->>>>>>> f07ef372bb475f34a776d8c10447cb8d56388a7e
