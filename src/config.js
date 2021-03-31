84830c611655373fae3b3f35549d9e63972e7e9b
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2021-03-31 15:54:04 -0700

    origin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 7000,
+  NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL || 'postgresql://postgres@localhost/saneful',
-  JWT_SECRET: process.env.JWT_SECRET || '106816c9-bcb5-4a05-9d51-4dda16a357c3',
+    process.env.DATABASE_URL ||
+    "postgres://mijdlipq:1ZpBnHYLWwFUkWlJngWpQiEFjUsNQYG5@kashin.db.elephantsql.com:5432/mijdlipq",
+  JWT_SECRET: process.env.JWT_SECRET || "106816c9-bcb5-4a05-9d51-4dda16a357c3",
 };
